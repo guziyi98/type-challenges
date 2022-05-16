6e89ffb97c95c1bc22ddb4b7fcb3f1dc0d56feec
guziyi <guziyi@plantdata.cn>
2022-05-16 17:52:23 +0800

    feat: add  tupleToObject

---
+++ 00011-easy-tuple-to-object/template.ts
@@ -0,0 +1,20 @@
+type TupleToObject<T extends readonly any[]> = {
+    [P in T[number]] : P
+}
+
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+
+type str = TupleToObject<typeof tuple>
+
+// js
+function tupleToObject (arr) {
+    // body
+    const obj = {}
+    arr.forEach(element => {
+        obj[element] = element
+    });
+    return obj
+}
+
+// 1.返回一个对象
+// 2.遍历array T[number]
---
+++ 00011-easy-tuple-to-object/test-case.ts
@@ -0,0 +1,34 @@
+import type { Equal, Expect } from "@type-challenges/utils";
+
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+
+type r = typeof tuple;
+
+// 字面量类型
+let str = "123";
+type str = typeof str;
+
+const strConst = "234";
+type sc = typeof strConst;
+
+type cases = [
+  Expect<
+    Equal<
+      TupleToObject<typeof tuple>,
+      {
+        tesla: "tesla";
+        "model 3": "model 3";
+        "model X": "model X";
+        "model Y": "model Y";
+      }
+    >
+  >
+];
+
+// @ts-expect-error
+type error = TupleToObject<[[1, 2], {}]>;
+
+// const let js世界
+// type interface type世界
+
+// 1.typeof 把js的世界抓换为ts世界
